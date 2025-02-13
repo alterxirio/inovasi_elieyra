5a7aecd6f391a809b0418dc0a1ec72788b9c86d6
aqif <aqifimran071130@gmail.com>
2025-02-13 17:09:46 +0800

    navbar half

--- SportSync/frontend/navbar.php
+++ SportSync/frontend/navbar.php
@@ -3,11 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>s
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
     <title>Document</title>
+    <style>
+        body{
+            
+        }
+        .navbar{
+            display: flex;
+            justify-content: space-between;
+            padding: 20px 50px;
+            background-color: #007ba7;
+        }
+        a{
+            color:#ffffff ;
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
+            padding: 0px 20px;
+        }
+        .link{
+            background-color: red;
+            text-align: center;
+        }
+    </style>
 </head>
 <body>
-    <p>Ba ba ba banana </p>
-    
+    <div class="navbar">
+        <div class="left">
+            <a href=""><img src="" alt=""></a>
+        </div>  
+        <div class="right">
+            <div class="link">
+                <a href="">New Team</a>
+                <a href="">text</a>
+                <a href="">Account</a>
+            </div>
+        </div>
+    </div>
+    <!-- <p>Ba ba ba banana </p> -->
 </body>
 </html>
+<!-- #007ba7 -->
+<!-- #104271 -->
+<!-- #ffffff -->
--- SportSync/index.php
+++ SportSync/index.php
@@ -6,7 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    <p>MUHAMMAD AQIF IMRANNNNNN JANGANNNNN LAAAAAA MERAJUKKKKKKKKK</p>
-    <p>MAJUKK</p>
+    <!-- <p>MUHAMMAD AQIF IMRANNNNNN JANGANNNNN LAAAAAA MERAJUKKKKKKKKK</p>
+    <p>MAJUKK</p> -->
 </body>
 </html>
