cbe912c12663b453c39c700874836f7bb924b366
aqif <aqifimran071130@gmail.com>
2025-02-16 01:36:30 +0800

    asdf

--- SportSync/index.php
+++ SportSync/index.php
@@ -4,9 +4,59 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./css/login.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
 </head>
 <body>
     <!-- <p>MUHAMMAD AQIF IMRANNNNNN JANGANNNNN LAAAAAA MERAJUKKKKKKKKK</p>
     <p>MAJUKK</p> -->
+     <center>
+        <div class="main">
+
+            <div class="left">
+
+                <div class="left-top">
+
+                    <h3>Hello!</h3>
+                    <p>Sign in into your account</p>
+
+                </div>
+                <div class="left-bottom">
+
+                    <form action="" method="post" >
+
+                        <label for="">Username</label>
+                        <br>
+                        <input type="text" name="username" placeholder="" >
+
+                        <br>
+
+                        <label for="">Password</label>
+                        <br>
+                        <input type="password" name="password" placeholder="" >
+
+                        <br>
+
+                        <center>
+                            <button type="submit">Submit</button>
+                        </center>
+                    
+                        <br>
+
+                        <label for="">Dont have an account? <a href="">Sign Up</a></label>
+
+                    </form>
+
+                </div>
+            </div>
+
+            <div class="right">
+                
+            </div>
+        </div>
+    </center>
+
 </body>
 </html>
