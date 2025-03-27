64d5bac3af01424d54a5776056a07aece6330fca
Elieyraester <m-10097517@moe-dl.edu.my>
2025-03-27 10:00:49 +0800

    bduduhbfuadfni

---
+++ SportSync/frontend/introduction.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Introduction</title>
+    <link rel="stylesheet" href="../css/introduction.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+
+    <?php
+        include('../frontend/navbar.php');
+        include('../config/config.php');
+        session_start();
+    ?>
+
+    <div class="banner">
+    <img src="..\media\Ball animation.gif" alt="SportSync">
+    </div>
+</body>
+</html>
